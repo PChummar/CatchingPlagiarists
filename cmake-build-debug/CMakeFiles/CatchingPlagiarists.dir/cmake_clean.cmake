fb34b7537889a5bccaf7a6df722349a8253e9e96
Prajwal Chummar <pc24495@utexas.edu>
2018-12-02 20:04:29 -0600

    First Commit

---
+++ cmake-build-debug/CMakeFiles/CatchingPlagiarists.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CatchingPlagiarists.dir/plagiarismCatcher.cpp.o"
+  "CatchingPlagiarists.pdb"
+  "CatchingPlagiarists"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CatchingPlagiarists.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- plagiarismCatcher.cpp
+++ plagiarismCatcher.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <cstdlib>
+#include <fstream>
 
 using namespace std;
 
@@ -24,18 +26,51 @@ int getdir (string dir, vector<string> &files)
     return 0;
 }
 
-int main(int argc, char *argv[])
+//Takes in a vector of words, processes them, applies the hashing function, and edits the hash table
+void hashFunction(vector<string> &key, vector<vector <int>> hashTable){
+    //Just leaving this blank for now, we can change it later
+}
+
+int main()
 {
+    //ordinarily would be a command prompt, keeping it simple for debugging, make sure you use the path for your own test folder
+    string dir = "Insert string here";
+    //number of words in a word chunk, also would normally be a command prompt
+    int numWords = 5;
 
-    string dir = argv[1];
-    int wordSeq = atoi(argv[2]);
-
+    vector<vector <int>> hashTable = vector<vector<int>>();
+    //each entry of this vector will hold the name of a file in the directory being processed
     vector<string> files = vector<string>();
 
+    //fills the file vector with the names of the files in the directory
     getdir(dir,files);
 
+    //prints the inex of each file in the file vector along with the file name
     for (unsigned int i = 0;i < files.size();i++) {
         cout << i << files[i] << endl;
+    }
+
+    //iterates through each file name
+    for(int i = 2; i < files.size(); i++){
+        string currentFile = files[i]; //Holds the current file
+        ifstream file; //input stream for the current file
+        file.open(currentFile);
+        string word;
+        char x;
+        word.clear();
+        bool flag = true;
+        vector<string> words;
+        for(int j = 0; j < numWords; j++){
+            if(file >> word){
+                words.push_back(word);
+            }
+            else{
+                j = numWords;
+            }
+        }
+        if(words.size() == numWords){
+            hashFunction(numWords, hashTable);
+        }
     }
     return 0;
 }
