d79674d92146a120c12a409a66ec46bbaba34d6c
Prajwal Chummar <pc24495@utexas.edu>
2018-12-06 13:26:44 -0600

    Final Commit

--- plagiarismCatcher.cpp
+++ plagiarismCatcher.cpp
@@ -12,18 +12,34 @@ using namespace std;
 typedef struct StackNode {
     int index;
     struct StackNode *next;
+
 } StackNode;
 
 typedef struct StackLL {
     struct StackNode *top;
+    StackLL(){
+        top = NULL;
+    }
+    void push(int a){
+            StackNode* add = new StackNode;
+            add->index = a;
+            add->next = top;
+            top = add;
+    }
 } StackLL;
 
-
+void update(vector< vector<int> > &matrix, int first, int second){
+    if(first < second){
+        matrix[first][second]++;
+    }
+    if (second < first){
+        matrix[second][first]++;
+    }
+}
 
 /*function... might want it in some class?*/
 int getdir (string dir, vector<string> &files)
 {
-    int HASH_LENGTH = 10000;
     DIR *dp;
     struct dirent *dirp;
     if((dp  = opendir(dir.c_str())) == NULL) {
@@ -39,18 +55,29 @@ int getdir (string dir, vector<string> &files)
 }
 
 //Takes in a vector of words, processes them, applies the hashing function, and edits the hash table
-void hashFunction(vector<string> &key, vector<vector <int>> hashTable){
-    //Just leaving this blank for now, we can change it later
+void hashFunction(vector<string> &key, StackLL hashTable[], int index){
+    string wordEdit = "";
+    for(int i = 0; i<key.size(); i++){
+        wordEdit += key[i];
+    }
+    char x;
+    int hash = 0;
+    for(int j= 0; j<wordEdit.length(); j++){
+        hash += wordEdit.at(j);
+    }
+    hashTable[hash].push(index);
 }
 
-int main()
+int main(int argc, char *argv[])
 {
+    int HASHLENGTH = 100000;
     //ordinarily would be a command prompt, keeping it simple for debugging, make sure you use the path for your own test folder
-    string dir = "/Users/bharathychummar/Documents/EE312/CatchingPlagiarists/cmake-build-debug/CMakeFiles/TestDocuments";
+    string dir = argv[1];
     //number of words in a word chunk, also would normally be a command prompt
-    int numWords = 5;
+    int numWords = atoi(argv[2]);
 
-    vector<vector <int>> hashTable = vector<vector<int>>();
+    int numSimilarities = atoi(argv[3]);
+    StackLL hashTable[HASHLENGTH];
     //each entry of this vector will hold the name of a file in the directory being processed
     vector<string> files = vector<string>();
 
@@ -84,11 +111,36 @@ int main()
         }
         if(words.size() == numWords){ //effectively checks if the input file has at least n words
             //if it doesn't, we don't have to do anything
-            hashFunction(words, hashTable); //hashes the initial n words
+            hashFunction(words, hashTable, i); //hashes the initial n words
             while(file >> word){ //loops while file has a next word and adds that word
                 //if it does, remove the first word from the words vector, and hashes the remaining chunk
+                words.push_back(word);
                 words.erase(words.begin());
-                hashFunction(words, hashTable);
+                hashFunction(words, hashTable, i);
+            }
+        }
+    }
+    vector<vector<int>> matrix(files.size(), vector<int>(files.size(), 0));
+    for (int i = 0; i < HASHLENGTH; i++){
+        StackNode* curr = hashTable[i].top;
+        if(curr != NULL){
+            StackNode *second = curr->next;
+            if(second != NULL){
+                while(second != NULL){
+                    while(second != NULL){
+                        update(matrix, curr->index, second->index);
+                        second = second->next;
+                    }
+                    curr = curr->next;
+                    second = curr->next;
+                }
+            }
+        }
+    }
+    for(int i = 0; i < files.size(); i++){
+        for(int j = i + 1; j < files.size(); j++){
+            if (matrix[i][j] > numSimilarities) {
+                cout << matrix[i][j] << " " << files[i] << " " << files[j] << endl;
             }
         }
     }
