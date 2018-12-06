5f1db9174ab6caacd671531e5bead169acf9f07a
Prajwal Chummar <pc24495@utexas.edu>
2018-12-06 10:50:52 -0600

    Second Commit

--- plagiarismCatcher.cpp
+++ plagiarismCatcher.cpp
@@ -9,9 +9,21 @@
 
 using namespace std;
 
+typedef struct StackNode {
+    int index;
+    struct StackNode *next;
+} StackNode;
+
+typedef struct StackLL {
+    struct StackNode *top;
+} StackLL;
+
+
+
 /*function... might want it in some class?*/
 int getdir (string dir, vector<string> &files)
 {
+    int HASH_LENGTH = 10000;
     DIR *dp;
     struct dirent *dirp;
     if((dp  = opendir(dir.c_str())) == NULL) {
@@ -20,7 +32,7 @@ int getdir (string dir, vector<string> &files)
     }
 
     while ((dirp = readdir(dp)) != NULL) {
-        files.push_back(string(dirp->d_name));
+        files.push_back(dir + "/" + string(dirp->d_name));
     }
     closedir(dp);
     return 0;
@@ -34,7 +46,7 @@ void hashFunction(vector<string> &key, vector<vector <int>> hashTable){
 int main()
 {
     //ordinarily would be a command prompt, keeping it simple for debugging, make sure you use the path for your own test folder
-    string dir = "Insert string here";
+    string dir = "/Users/bharathychummar/Documents/EE312/CatchingPlagiarists/cmake-build-debug/CMakeFiles/TestDocuments";
     //number of words in a word chunk, also would normally be a command prompt
     int numWords = 5;
 
@@ -55,21 +67,29 @@ int main()
         string currentFile = files[i]; //Holds the current file
         ifstream file; //input stream for the current file
         file.open(currentFile);
-        string word;
-        char x;
-        word.clear();
-        bool flag = true;
-        vector<string> words;
+        if(!file.is_open()){
+            cout << "File can not be accessed" <<endl;
+        }
+        string word; //will hold each word in the input file successively, to be placed into a vector
+        word.clear(); //not totally sure why we need this tbh
+        //bool flag = true; //
+        vector<string> words; //vector that will hold a chunk of n words (decided by command prompt)
         for(int j = 0; j < numWords; j++){
-            if(file >> word){
-                words.push_back(word);
+            if(file >> word){ //checks if file has a next word
+                words.push_back(word); //adds the word to the vector
             }
             else{
-                j = numWords;
+                j = numWords; //breaks out of the for loop
             }
         }
-        if(words.size() == numWords){
-            hashFunction(numWords, hashTable);
+        if(words.size() == numWords){ //effectively checks if the input file has at least n words
+            //if it doesn't, we don't have to do anything
+            hashFunction(words, hashTable); //hashes the initial n words
+            while(file >> word){ //loops while file has a next word and adds that word
+                //if it does, remove the first word from the words vector, and hashes the remaining chunk
+                words.erase(words.begin());
+                hashFunction(words, hashTable);
+            }
         }
     }
     return 0;
